0562ae900d2076089cd3030011e799f70b61693c
Andrea Walsh <61071442+awalsh0517@users.noreply.github.com>
2021-01-14 15:11:52 -0500

    Hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,34 @@
 const router = require('express').Router()
 const User = require('../model/User')
+const bcrypt = require('bcryptjs')
+const { registerValidation } = require('../validation')
 
-// Validation
-const Joi = require('joi')
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required()
-})
-
-router.post('/register', (req, res) => {
-
+router.post('/register', async (req, res) => {
   // validate data before make user
-  const validation = schema.validate(req.body)
-  res.send(validation)
+  const { error } = registerValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password
-  // })
-  // try {
-  //   const savedUser = await user.save()
-  //   res.send(savedUser)
-  // } catch (err) {
-  //   res.status(400).send(err)
-  // }
+  //Check if user is in database
+  const emailExists = await User.findOne({ email: req.body.email })
+  if (emailExists) return res.status(400).send('Email aready exists')
+
+  //Hash the Password
+  const salt = await bcrypt.gentSalt(10)
+  const hashedPassword = await bcrypt.hash(rew.body.password, salt)
+
+
+  //Create a new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 // router.post('/login')
