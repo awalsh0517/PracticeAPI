3345cd86e23bc84db335befa95c31527d20217b9
Andrea Walsh <61071442+awalsh0517@users.noreply.github.com>
2021-01-14 12:17:04 -0500

    Beginings of an API project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+
+// Imoport routes
+const authRoute = require('./routes/auth')
+
+dotenv.config()
+
+// connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+
+  () => console.log('connected to db'))
+
+//Middleware
+app.use(express.json())
+
+// Route Middlewares
+app.use('/api/user', authRoute)
+
+app.listen(3000, () => console.log('Server is running'))
